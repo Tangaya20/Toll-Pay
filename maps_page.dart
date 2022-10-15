6c46794e72a9330c80a2b02d2ceb115f35db0dff
Tangaya20 <115025402+Tangaya20@users.noreply.github.com>
2022-10-15 13:24:51 +0300

    made some changes

---
+++ maps_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MyMap extends StatefulWidget {
+  const MyMap({Key? key}) : super(key: key);
+
+  @override
+  State<MyMap> createState() => _MyMapState();
+}
+
+class _MyMapState extends State<MyMap> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(0.0451, 32.4427);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 14.0,
+          ), 
+          ),
+    );
+  }
+}
